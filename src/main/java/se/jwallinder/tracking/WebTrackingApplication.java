502857be60522f0a3c8e8f080f549b54c2f1a6ce
johan wallinder <johan.wallinder@gmail.com>
2020-05-15 18:13:57 +0200

    init

---
+++ src/main/java/se/jwallinder/tracking/WebTrackingApplication.java
@@ -0,0 +1,13 @@
+package se.jwallinder.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/jwallinder/tracking/WebTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jwallinder.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
